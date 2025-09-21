3ed954300bd16fe6c0b470298f916c21a3f4a395
Gulzar Hussain <gulzarhussain@Gulzars-MacBook-Air.local>
2025-09-22 02:41:47 +0530

    add some missing

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const connectionDB = require('./src/db/db.js');
 connectionDB();
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is running on port 3000");
 })
