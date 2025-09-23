2933466b55dfe224ee6f373fa526b13396fd7c58
Gulzar Hussain <gulzarhussain@Gulzars-MacBook-Air.local>
2025-09-23 19:29:28 +0530

    add admin panel

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose');
-
-// const projectSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     description: { type: String, required: true },
-//     link: { type: String },
-//     image: { type: String }
-// }, { timestamps: true });
-
-// const Project = mongoose.model('Project', projectSchema);
-
-// module.exports = Project;
-
-// import mongoose from 'mongoose';
 const projectSchema = new mongoose.Schema({
     name: { type: String, required: true },
     technology: { type: [String], required: true },
